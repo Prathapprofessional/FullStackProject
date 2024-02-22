b54ea047ac1dfc436e8265905821494ed8f2b0c9
Prathap Dhanasekaran <prathapprof@gmail.com>
2024-02-22 18:16:20 +0100

    sending json data to postman via postmapping

--- src/main/java/com/example/fullstackproject/Controller/TaskController.java
+++ src/main/java/com/example/fullstackproject/Controller/TaskController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TaskController {
     @GetMapping("/hello")
@@ -13,8 +16,12 @@ public class TaskController {
     }
 
     @PostMapping("/api/work")
-    public String post()
-    {
-        return "Hello post";
+    public List<String> createTask(){
+        List<String> users = new ArrayList<>();
+        users.add("Adam");
+        users.add("Bob");
+        users.add("Charles");
+        return users;
     }
+
 }
